ddf95a17a104efae556f01658ca01066f1df8e3a
Subhash Mani <subhmani4631@gmail.com>
2024-06-11 22:11:32 +0530

    add validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,6 +2,7 @@ import { useRef,useState } from "react";
 
 const SimpleInput=(props)=>{
     const [userInput,setUserInput]=useState("");
+    const[inputIsValid,setInputIsValid]=useState(" ");
     const userInputRef=useRef("");
     const changeHandler=(event)=>{
         setUserInput(event.target.value);
@@ -9,14 +10,21 @@ const SimpleInput=(props)=>{
     }
     const submitHandler=(event)=>{
         event.preventDefault();
+        if(userInput.trim().length===0){
+            setInputIsValid(false)
+            return
+        }
+       
         console.log("Submit handler called",userInput,userInputRef.current.value);
         setUserInput("");
     }
+    const controlClass=inputIsValid?"form-control":"form-control invalid";
     return(
         <form onSubmit={submitHandler}>
-            <div className="form-control">
+            <div className={controlClass}>
                 <label>User Name</label>
                 <input value={userInput} ref={userInputRef} className="form-input" onChange={changeHandler} type="text" id="username"/>
+                {!inputIsValid && <p className="error-text">*User name is required</p>}
             </div>
             <div className="form-action">
                 <button type="submit">Submit</button>
