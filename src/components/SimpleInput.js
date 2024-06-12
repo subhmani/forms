f1a2cbed89d4c0b5d743a761b06dca5ac931b5c6
Subhash Mani <subhmani4631@gmail.com>
2024-06-12 18:25:47 +0530

    Validity check on user input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,48 +2,57 @@ import { useEffect, useRef,useState } from "react";
 
 const SimpleInput=(props)=>{
     const [userInput,setUserInput]=useState("");
-    const[inputIsValid,setInputIsValid]=useState(false);
+    //const[inputIsValid,setInputIsValid]=useState(false);
     const[inputTouched,setInputTouched]=useState(false);
-    const userInputRef=useRef("");
-    useEffect(()=>{
+    const inputIsValid=userInput.trim().length!==0;
+    const inputIsInValid=!inputIsValid && inputTouched;
+    //const userInputRef=useRef("");
+   /*  useEffect(()=>{
         if(inputIsValid){
             console.log("use effect handled")
         }
-    },[inputIsValid])
+    },[inputIsValid]) */
     const changeHandler=(event)=>{
         setUserInput(event.target.value);
-        console.log("User Input change handler",event.target.value);
+       /*  if(event.target.value.trim().length > 0){
+            setInputIsValid(true);
+        }
+        console.log("User Input change handler",event.target.value); */
     }
     const onBlurHandler=(event)=>{
         setInputTouched(true);
-        if(event.target.value.trim().length===0){
+       /*  if(event.target.value.trim().length===0){
             setInputIsValid(false);
         }
         else{
             setInputIsValid(true);
-        }
+        } */
     }
     const submitHandler=(event)=>{
         event.preventDefault();
         setInputTouched(true);
-        if(userInput.trim().length===0){
+       /*  if(userInput.trim().length===0){
             setInputIsValid(false)
             return
+        } */
+        if(!inputIsInValid){
+            return
         }
-        setInputIsValid(true);
+       // setInputIsValid(true);
        
-        console.log("Submit handler called",userInput,userInputRef.current.value);
+        console.log("Submit handler called",userInput);
         setUserInput(" ");
+        setInputTouched(false);
     }
-    const controlClass=inputIsValid || !inputTouched?"form-control":"form-control invalid";
+    const controlClass=inputIsInValid ?"form-control invalid":"form-control";
     return(
         <form onSubmit={submitHandler}>
             <div className={controlClass}>
                 <label>User Name</label>
-                <input value={userInput} ref={userInputRef} className="form-input" 
+                <input value={userInput} className="form-input" 
                 onChange={changeHandler} onBlur={onBlurHandler} 
                 type="text" id="username"/>
-                {!inputIsValid && inputTouched && <p className="error-text">*User name is required</p>}
+                {inputIsInValid && <p className="error-text">*User name is required</p>}
             </div>
             <div className="form-action">
                 <button type="submit">Submit</button>
