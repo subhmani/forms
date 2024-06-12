4a96c12c23e2fd19a340dbfddf14c8b47fcf0b3b
Subhash Mani <subhmani4631@gmail.com>
2024-06-12 17:19:56 +0530

    Handling on touch and blur

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,30 +1,38 @@
-import { useRef,useState } from "react";
+import { useEffect, useRef,useState } from "react";
 
 const SimpleInput=(props)=>{
     const [userInput,setUserInput]=useState("");
-    const[inputIsValid,setInputIsValid]=useState(" ");
+    const[inputIsValid,setInputIsValid]=useState(false);
+    const[inputTouched,setInputTouched]=useState(false);
     const userInputRef=useRef("");
+    useEffect(()=>{
+        if(inputIsValid){
+            console.log("use effect handled")
+        }
+    },[inputIsValid])
     const changeHandler=(event)=>{
         setUserInput(event.target.value);
         console.log("User Input change handler",event.target.value);
     }
     const submitHandler=(event)=>{
         event.preventDefault();
+        setInputTouched(true);
         if(userInput.trim().length===0){
             setInputIsValid(false)
             return
         }
+        setInputIsValid(true);
        
         console.log("Submit handler called",userInput,userInputRef.current.value);
-        setUserInput("");
+        setUserInput(" ");
     }
-    const controlClass=inputIsValid?"form-control":"form-control invalid";
+    const controlClass=inputIsValid || !inputTouched?"form-control":"form-control invalid";
     return(
         <form onSubmit={submitHandler}>
             <div className={controlClass}>
                 <label>User Name</label>
                 <input value={userInput} ref={userInputRef} className="form-input" onChange={changeHandler} type="text" id="username"/>
-                {!inputIsValid && <p className="error-text">*User name is required</p>}
+                {!inputIsValid && inputTouched && <p className="error-text">*User name is required</p>}
             </div>
             <div className="form-action">
                 <button type="submit">Submit</button>
