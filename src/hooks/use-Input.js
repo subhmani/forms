47d170cb5994f074c477961379e13a1573876a9d
Subhash Mani <subhmani4631@gmail.com>
2024-06-18 21:54:14 +0530

    use Reducer

--- src/hooks/use-Input.js
+++ src/hooks/use-Input.js
@@ -1,20 +1,38 @@
-import { useState } from "react";
+import { type } from "@testing-library/user-event/dist/type";
+import { act, useReducer, useState } from "react";
 
 const useInput=(validateInput)=>{
-    const [input,setInput]=useState("");
-    const[inputTouched,setInputTouched]=useState(false);
-    const inputIsValid=validateInput(input);
-    const inputIsInValid=!inputIsValid && inputTouched;
+    const initialInputState={value:'',isTouched:false};
+    const inputStateReducer=(state,action)=>{
+        if(action.type==='INPUT'){
+            return {value:action.value,isTouched:state.isTouched}
+        }
+        else if(action.type==='TOUCHED'){
+            return{isTouched:action.value,value:state.value} 
+        }
+        else if(action.type==="RESET"){
+            return initialInputState
+        }
+        return initialInputState
+    }
+    const[inputState,dispatch]=useReducer(inputStateReducer,initialInputState); 
+    //const [input,setInput]=useState("");
+    //const[inputTouched,setInputTouched]=useState(false);
+    const inputIsValid=validateInput(inputState.value);
+    const inputIsInValid=!inputIsValid && inputState.isTouched;
     const changeHandler=(event)=>{
-        setInput(event.target.value);
+        dispatch({type:'INPUT',value:event.target.value})
+        //setInput(event.target.value);
     }
     const blurHandler=(event)=>{
-        setInputTouched(true);
+        dispatch({type:'TOUCHED',value:true})
+        //setInputTouched(true);
     }
     const reset=()=>{
-        setInput(" ");
-        setInputTouched(false);
+        dispatch({type:'RESET'})
+       // setInput(" ");
+       // setInputTouched(false);
     }
-    return {input,inputIsValid,inputIsInValid,changeHandler,blurHandler,reset}
+    return {input:inputState.value,inputIsValid,inputIsInValid,changeHandler,blurHandler,reset}
 }
 export default useInput;
