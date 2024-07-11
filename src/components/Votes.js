b9475250de7b4e6e6664f97b05e020e36378ac34
Subhash Mani <subhmani4631@gmail.com>
2024-07-11 19:05:30 +0530

    Dispatching Action in component

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,11 +1,26 @@
-import { useSelector } from "react-redux";
+import { type } from "@testing-library/user-event/dist/type";
+import { useSelector,useDispatch } from "react-redux";
 const Votes=()=>{
     const votes = useSelector(state=>state.votes)
+    const dispatch=useDispatch();
+    const incrementHandler=()=>{
+        dispatch({type:"Increment",value:10})
+    }
+    const decrementHandler=()=>{
+        dispatch({type:"Decrement"})
+    }
+    const addBy10Handler=()=>{
+        dispatch({type:'Increase',value:10})
+    }
     const toggleVotesHandler=()=>{}
+
     return (
         <div className="votes">
             <h1>Redux Votes</h1>
             <div className="value"> Number of votes</div>
+            <button onClick={incrementHandler}>Increment</button>
+            <button onClick={decrementHandler}>Decrement</button>
+            <button onClick={addBy10Handler}>Add 10 votes</button>
             <button onClick={toggleVotesHandler}>Show/Hide Votes</button>
         </div>
     )
--- src/store/votes-store.js
+++ src/store/votes-store.js
@@ -1,10 +1,14 @@
 import { createStore } from  'react-redux';
+
 const reducer=(state={votes:0},action)=>{
     if(action.type==="Increment"){
         return {votes:state.votes+1}
     }else if(action.type==="Decrement"){
     return {votes:state.votes-1}
     }
+    else if(action.type==="Increase"){
+        return {votes:state.votes+action.value}
+        }
 }
 const voteStore=createStore(reducer);
 export default voteStore;
