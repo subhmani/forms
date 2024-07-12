44dc532b89711041e6ccb11648291b971e7136df
Subhash Mani <subhmani4631@gmail.com>
2024-07-12 16:27:13 +0530

    redux toolkit

--- src/store/votes-store.js
+++ src/store/votes-store.js
@@ -1,7 +1,9 @@
 import { createStore } from  'redux';
+import { createSlice } from '@reduxjs/toolkit';
 const initialState={votes:0,toggle:true}
 const reducerFunction=(state=initialState,action)=>{
     if(action.type==="Increment"){
+
         return {votes:state.votes+1,toggle:state.toggle}
     }else if(action.type==="Decrement"){
     return {votes:state.votes-1,toggle:state.toggle}
@@ -13,5 +15,13 @@ const reducerFunction=(state=initialState,action)=>{
         return {votes:state.votes,toggle:!state.toggle}
     }    
 }
+reducer:{
+    Increment:(state)=>state.votes++;
+    Decrement:(state)=>state.votes--;
+    Increase:(state)=>state.votes;
+    Toggle:(state)=>!state.toggle;
+}
+createSlice(voteSlice, initialState, reducer)
 const voteStore=createStore(reducerFunction);
+
 export default voteStore;
